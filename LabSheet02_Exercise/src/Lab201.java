62f0e529c8de3b0f92f46eb18e31ac23ccc322ec
sirawit033-ops <sirawit033@gmail.com>
2026-07-07 17:26:43 +0700

    2513310116 Submit LabSheet02

---
+++ LabSheet02_Exercise/src/Lab201.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Lab201 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner scan = new Scanner(System.in); 
+		 ArrayList<String> numlist = new ArrayList<String>();
+		 numlist.add("Alice");
+		 numlist.add("Emma");
+		 numlist.add("Leon");
+		 numlist.add("John");
+		 System.out.println("Length = "+numlist.size());
+		 System.out.print(numlist);
+		 
+		 
+	}
+
+}
---
+++ LabSheet02_Exercise/src/Lab202.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Lab202 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[]  initial_numbers= {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		numbers.addAll(Arrays.asList(initial_numbers));
+		System.out.print(numbers);
+	}
+
+}
