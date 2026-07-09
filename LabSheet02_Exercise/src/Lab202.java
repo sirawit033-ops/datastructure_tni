60cec7d029d5b12d6f905de86a2ca3c8471d4466
sirawit033-ops <sirawit033@gmail.com>
2026-07-09 08:26:26 +0700

    Add files via upload

--- LabSheet02_Exercise/src/Lab202.java
+++ LabSheet02_Exercise/src/Lab202.java
@@ -1,14 +1,39 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Lab202 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Integer[]  initial_numbers= {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
-		ArrayList<Integer> numbers = new ArrayList<Integer>();
-		numbers.addAll(Arrays.asList(initial_numbers));
-		System.out.print(numbers);
-	}
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int[] initial_numbers = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
 
+        for (int num : initial_numbers) {
+            numbers.add(num);
+        }
+        System.out.println("ข้อมูลเริ่มต้นใน numbers: " + numbers);
+
+        System.out.print("ป้อนตัวเลขเพื่อเพิ่มต่อท้าย: ");
+        int appendNum = scanner.nextInt();
+        numbers.add(appendNum); // .add(value) จะเป็นการต่อท้ายอัตโนมัติ
+
+        int lastDigit = 6;
+        System.out.print("ป้อนตัวเลขเพื่อแทรกที่ตำแหน่ง " + lastDigit + ": ");
+        int insertNum = scanner.nextInt();
+        numbers.add(lastDigit, insertNum);
+
+        int secondLastDigit = 6;
+        numbers.remove(secondLastDigit);
+        System.out.println("-> ลบข้อมูลที่ตำแหน่ง " + secondLastDigit + " เรียบร้อยแล้ว");
+
+        int thirdLastDigit = 1;
+        int studyDate = 7;
+        numbers.set(thirdLastDigit, studyDate);
+        System.out.println("-> อัปเดตตำแหน่ง " + thirdLastDigit + " เป็นค่า " + studyDate + " เรียบร้อยแล้ว");
+
+        System.out.println("--- ผลลัพธ์สุดท้าย ---");
+        System.out.println("ข้อมูลใน ArrayList ของนักศึกษา: " + numbers);
+
+        scanner.close();
+    }
 }
