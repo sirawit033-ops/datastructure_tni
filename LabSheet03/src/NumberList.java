3d9ffa75d87e217a9f3a6268c363c9861e234b0c
sirawit033-ops <sirawit033@gmail.com>
2026-07-14 17:15:17 +0700

    2513310116 Submit LabSheet03

---
+++ LabSheet03/src/NumberList.java
@@ -0,0 +1,32 @@
+
+public class NumberList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList numlist = new LinkedList();
+		numlist.insert(0, 37);
+		numlist.insert(0, 7);
+		numlist.insert(0, 4);
+		numlist.insert(0, 16);
+		System.out.println(numlist.traversal());
+		
+		numlist.insert(25);
+		System.out.println(numlist.traversal());
+		
+		numlist.remove(0);
+		System.out.println(numlist.traversal());
+		
+		numlist.remove(2);
+		System.out.println(numlist.traversal());
+		
+		numlist.removeLastElement();
+		System.out.println(numlist.traversal());
+		
+		numlist.lenght();
+		System.out.println(numlist.traversal());
+		
+		numlist.clear();
+		System.out.println(numlist.traversal());
+		
+}
+}
