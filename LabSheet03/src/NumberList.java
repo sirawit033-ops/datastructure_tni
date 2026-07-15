5c38aa1927442d6bf0d86a30553536147cf8f522
sirawit033-ops <sirawit033@gmail.com>
2026-07-15 22:16:06 +0700

    2513310116 Submit LabSheet03

--- LabSheet03/src/NumberList.java
+++ LabSheet03/src/NumberList.java
@@ -1,35 +1,35 @@
 
 public class NumberList {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LinkedList numlist = new LinkedList();
-		numlist.insert(0, 37);
-		numlist.insert(0, 7);
-		numlist.insert(0, 4);
-		numlist.insert(0, 16);
-		System.out.println(numlist.traversal());
-		
-		numlist.insert(25);
-		System.out.println(numlist.traversal());
-		
-		numlist.remove(0);
-		System.out.println(numlist.traversal());
-		
-		numlist.remove(2);
-		System.out.println(numlist.traversal());
-		
-		numlist.removeLastElement();
-		System.out.println(numlist.traversal());
-		
-		System.out.println("Lenght"+numlist.lenght());
-		System.out.println("Node 2 "+numlist.get(2));
-		
-		numlist.set(1, 100);
-		System.out.println(numlist.traversal());
-		
-		numlist.clear();
-		System.out.println(numlist.traversal());
-		
-}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        LinkedList numlist = new LinkedList();
+        numlist.insert(0, 37);
+        numlist.insert(0, 7);
+        numlist.insert(0, 4);
+        numlist.insert(0, 16);
+        System.out.println(numlist.traversal());
+
+        numlist.insert(25);
+        System.out.println(numlist.traversal());
+
+        numlist.remove(0);
+        System.out.println(numlist.traversal());
+
+        numlist.remove(2);
+        System.out.println(numlist.traversal());
+
+        numlist.removeLastElement();
+        System.out.println(numlist.traversal());
+
+        System.out.println("Lenght"+numlist.lenght());
+        System.out.println("Node 2 "+numlist.get(2));
+
+        numlist.set(1, 100);
+        System.out.println(numlist.traversal());
+
+        numlist.clear();
+        System.out.println(numlist.traversal());
+
+    }
 }
